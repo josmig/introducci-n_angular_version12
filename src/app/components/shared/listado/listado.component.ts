1df242a02a1305e928f7740cd23f4dc171510b0e
josmig <pantigosomiguel59@gmail.com>
2021-07-31 23:39:20 -0500

    uso de ngfor && ngif

--- src/app/components/shared/listado/listado.component.ts
+++ src/app/components/shared/listado/listado.component.ts
@@ -5,5 +5,17 @@ import { Component} from '@angular/core';
   templateUrl: './listado.component.html'  
 })
 export class ListadoComponent {
+  heroes: string[] = ['Spiderman','Thor','IronMan','Hawkaye','Capitan America'];  
+  
+  heroeBorrado: string = '';
+
+  delete(){
+    /* console.log('Borrando...');     */
+    /* this.heroes.splice(i,1); */
+    this.heroeBorrado =this.heroes.shift() || '';
+    //shift devuelve un string o undifened por eso es recomendable mandar tambien un array vacio
+    
+    
+  }
 
 }
