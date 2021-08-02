0a9280a46641c4f070330be5db17a32904449bdb
josmig <pantigosomiguel59@gmail.com>
2021-08-01 23:57:33 -0500

    modulo DBZ + component + add app.module.ts(main)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 //Modulos
 import { HeroesModule } from './components/heroes/heroes.module';
 import { ContadorModule } from './components/contador/contador.module';
-
+import { DbzModule } from './components/dbz/dbz.module';
 
 
 @NgModule({
@@ -16,7 +16,8 @@ import { ContadorModule } from './components/contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
