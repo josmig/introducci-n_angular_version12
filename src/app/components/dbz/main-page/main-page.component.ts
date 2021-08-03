b98417b4937b9c4a99cade0ee1d76f66d6e1dc44
josmig <pantigosomiguel59@gmail.com>
2021-08-03 00:15:01 -0500

    ngModel + mostando lista de personajes

--- src/app/components/dbz/main-page/main-page.component.ts
+++ src/app/components/dbz/main-page/main-page.component.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+  personajes: Personaje [] = [
+    {
+      nombre:'Goku',
+      poder:15000
+    },
+    {
+      nombre:'Vegetta',
+      poder:13000
+    }
+  ]
+  nuevo: Personaje={
+    nombre:'',
+    poder:0
+  }  
 
-  /* event: any */
   agregar(){
-    /* event.preventDefault(); */
-    console.log('Esta es una prueba');
-    
-    
-  }
+    if(this.nuevo.nombre.trim().length === 0 ){return;}    
 
+    //agregando al array
+    this.personajes.push(this.nuevo);
+    //borando el form para agregar otro
+    this.nuevo = {
+      nombre:'',
+      poder:0
+    }
+    console.log(this.nuevo);      
+  }
+  
 }
